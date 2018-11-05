47b2e6c59aa4cb709007b1e4ecff519f4c4a744b
Todd Enders <tenders64@gmail.com>
2018-11-05 09:55:14 -0800

    Removed portfolio solution

--- 3.Javascript/3.BasicFoundation2/solution.js
+++ 3.Javascript/3.BasicFoundation2/solution.js
@@ -104,36 +104,96 @@ function incrementSeconds(arr){
     return arr;
 }
 
-console.log(incrementSeconds([1,2,3,4,5]));
+// console.log(incrementSeconds([1,2,3,4,5]));
 
 // Previous Lengths - You are passed an array containing strings.  Working within that same array, replace each string with a number - the length of the string at previous array index - and return the array.  For example, previousLengths(["hello", "dojo", "awesome"]) should return ["hello", 5, 4].
 
+function previousLengths(arr){
+    for (var i = arr.length-1; i > 0; i--){
+        arr[i] = arr[i-1].length;
+    }
+    return arr;
+}
 
-
+// console.log(previousLengths(["hello", "dojo", "awesome"]));
 
 // Add Seven to Most - Build function that accepts array. Return a new array with all values except first, adding 7 to each. Do not alter the original array.
 
+function sevenMore(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++){
+        newArr.push(arr[i]+7);
+    }
+    return newArr;
+}
 
-
+// console.log(sevenMore([0,1,2,3]));
 
 // Reverse Array - Given an array, write a function that reverses values, in-place.  Example: reverse([3,1,6,4,2]) return same array, containing [2,4,6,1,3].  Do this without creating an empty temporary array.  (Hint: you'll need to swap values).
 
+function reverse(arr){
+    for (var i = 0; i < arr.length/2; i++){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
 
+// console.log(reverse([3,1,6,4,2,5]));
 
 
 // Outlook: Negative - Given an array, create and return a new one containing all the values of the provided array, made negative (not simply multiplied by -1). Given [1,-3,5], return [-1,-3,-5].
 
+function negativeMaker(arr){
+    var newArr = [];
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] > 0){
+            arr[i] = arr[i]*-1;
+        }
+        newArr.push(arr[i]);
+    }
+    return newArr;
+}
 
+// console.log(negativeMaker([1,-3,5]));
 
+// Always Hungry - Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food".  If no array elements are "food", then print "I'm hungry" once. Example: [1,"food",true, "food"] ==> "yummy" "yummy". [1,2,4,"sleep"] ==> "I'm hungry"
 
-// Always Hungry - Create a function that accepts an array, and prints "yummy" each time one of the values is equal to "food".  If no array elements are "food", then print "I'm hungry" once.
+function hungry(arr){
+    var gotFood = false;
+    for (var i = 0; i < arr.length; i++){
+        if (arr[i] === "food"){
+            console.log("yummy");
+            gotFood = true;
+        }
+    }
+    if (gotFood == false){
+        console.log("I'm hungry");
+    }
+}
 
-
-
+// hungry([1,2,4,"sleep"]);
 
 // Swap Toward the Center - Given array, swap first and last, third and third-to-last, etc.  Input[true,42,"Ada",2,"pizza"] becomes ["pizza", 42, "Ada", 2, true].  Change [1,2,3,4,5,6] to [6,2,4,3,5,1].
 
+function swapTowardCenter(arr){
+    for (var i = 0; i < arr.length/2; i+=2){
+        var temp = arr[i];
+        arr[i] = arr[arr.length-1-i];
+        arr[arr.length-1-i] = temp;
+    }
+    return arr;
+}
 
-
+// console.log(swapTowardCenter([1,2,3,4,5,6]));
 
 // Scale the Array - Given an array arr and a number num, multiply all values in arr by num, and return the changed array arr.  For example, scaleArray([1,2,3],3) should return [3,6,9].
+
+function scaleArray(arr, num){
+    for (var i = 0; i < arr.length; i++){
+        arr[i] = arr[i]*num;
+    }
+    return arr;
+}
+console.log(scaleArray([1,2,3,4],4));
